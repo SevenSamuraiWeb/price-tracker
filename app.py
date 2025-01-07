39771e817aa95cf409a32115696707aca7a8141e
Nihaal Virgincar <nihaalvirgincar193@gmail.com>
2025-01-07 16:45:14 +0530

    trying to fix errors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from main import send_mail
 
 sys.path.append("C:\\Users\\nihaa\PycharmProjects\PythonProject")
 
-st.header("Flipkart price tracker")
+st.title("Flipkart price tracker")
 url = st.text_input("Enter the product url")
 email = st.text_input("Enter ur email")
 target_price = st.number_input("Enter target price",min_value=0.0,step=1.0)
@@ -21,11 +21,16 @@ headers = {
 if st.button("Track price:"):
     if email and target_price and url:
         with st.spinner("fetching price..."):
-            title,price = fetch_price(url,headers)
-            print(f"Product name:{title}")
-            print(f"Price:{price}")
-            if price <= target_price:
-                send_mail(url,title,email)
-                st.success(f"Price dropped to {price}Rs\nEmail has been sent")
-            else:
-                st.info(f"Price is {price}Rs. \nStill higher than target price")
+            try:
+                title,price = fetch_price(url,headers)
+                print(f"Product name:{title}")
+                print(f"Price:{price}")
+                if price <= target_price:
+                    send_mail(url,title,email)
+                    st.success(f"Price dropped to {price}Rs\nEmail has been sent")
+                else:
+                    st.info(f"Price is {price}Rs. \nStill higher than target price")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+    else:
+        st.warning("Please fill in all the fields.")
